8e0128cce6627f4104beb4ea197375d2360732bf
ivoorusso <ivan@setandforget.io>
2023-09-05 18:23:52 -0300

    Update Coaching

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Incidents', href: '#', current: true },
+  { name: 'Coaching', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
@@ -186,7 +186,7 @@ export default function App() {
         <div className="py-4 flex flex-col flex-1">
           <header className="mb-4">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Incidents</h1>
+              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Coaching</h1>
             </div>
           </header>
           <main className="flex flex-col flex-1">
